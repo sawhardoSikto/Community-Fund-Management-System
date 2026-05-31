3fe1cccc072253b81dc56697263f833ddc0f07c2
Sawhardo Biswas Sikto <22-47406-2@student.aiub.edu>
2026-05-31 17:21:22 +0600

    backend created

---
+++ ushaan-api/src/main.ts
@@ -0,0 +1,29 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({ origin: 'http://localhost:4000', credentials: true });
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Ushaan API')
+    .setDescription('Community Fund Management System')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
